c560551f6310ed4b2ee3d49c3185014ebe3a92f0
naveed-ul-llah <naveed@Innovative.net>
2026-01-14 15:48:47 +0500

    Footer4

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3c6242efea838cbd"; }
+/******/ 		__webpack_require__.h = function() { return "8022bb013ab3addc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
